d8759de3c920827f33853273a61200d6f174a98b
HurrayChak <hurray@findsolutiongroup.com>
2023-10-30 01:09:01 +0800

    Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import { api } from "~/trpc/server";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -19,10 +17,24 @@ export default async function Home() {
           {/* Signed out users get sign in button */}
           <SignInButton />
         </SignedOut>
-
+        <GetAllPost />
         {/* <CrudShowcase /> */}
       </div>
     </main>
+  );
+}
+
+async function GetAllPost() {
+  const allPost = await api.post.getAll.query();
+  console.log(allPost);
+  return (
+    <div className="w-full max-w-xs">
+      {allPost ? (
+        allPost?.map((post) => <div key={post.id}>{post.content}</div>)
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+    </div>
   );
 }
 
@@ -32,7 +44,7 @@ async function CrudShowcase() {
   return (
     <div className="w-full max-w-xs">
       {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+        <p className="truncate">Your most recent post: {latestPost.id}</p>
       ) : (
         <p>You have no posts yet.</p>
       )}
