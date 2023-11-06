f3eba0183d144778a39c90633cd84f38cb097da7
HurrayChak <hurray@findsolutiongroup.com>
2023-11-06 14:14:27 +0800

    Ratelimit in Endpoint

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,6 +7,8 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -14,6 +16,21 @@ const filterUserForClient = (user: User) => {
     username: user.username,
   };
 };
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const limitDuration = "10 s";
+const limitAmount = 3;
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(limitAmount, limitDuration),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -51,6 +68,14 @@ export const postRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success)
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+          message: `You can only create ${limitAmount} posts in ${limitDuration}`,
+        });
 
       const post = await ctx.db.post.create({
         data: {
