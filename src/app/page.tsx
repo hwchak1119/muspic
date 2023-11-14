1a60a4aa33cc33a60f9cd41529108e897fade1d6
HurrayChak <hurray@findsolutiongroup.com>
2023-11-15 02:22:45 +0800

    Remove async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Home",
 };
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
