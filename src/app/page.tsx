9e788613a41415ffc4c12592bf9b93f2295f8859
HurrayChak <hurray@findsolutiongroup.com>
2023-11-15 00:54:58 +0800

    Undo remove sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        {/* <SignedIn>
+        <SignedIn>
           <UserButton />
         </SignedIn>
         <SignedOut>
           <SignInButton />
-        </SignedOut> */}
+        </SignedOut>
         <PostList />
         <CreatePost />
       </div>
